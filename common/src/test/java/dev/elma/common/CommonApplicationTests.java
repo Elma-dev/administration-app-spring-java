736015f9dd171f800acaa381d238c6e879936873
Elma-dev <elmajjodi.abdeljalil@gmail.com>
2024-02-07 09:08:47 +0100

    add common module

---
+++ common/src/test/java/dev/elma/common/CommonApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.elma.common;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommonApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
